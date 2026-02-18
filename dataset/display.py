1c89d58fc1a5e29834a1b450652a7429118a358f
allthemollyoutake <guptamaalvika@gmail.com>
2026-02-19 03:13:38 +0530

    Update display.py
    added constraints

--- dataset/display.py
+++ dataset/display.py
@@ -1,4 +1,5 @@
 import pandas as pd
 url = "https://raw.githubusercontent.com/allthemollyoutake/Genetic-Mutation-Insight-System/refs/heads/main/dataset/clinvar_conflicting.csv"
-df = pd.read_csv(url)
+df = pd.read_csv(url, nrows=100)
 print(df.head())
+print(df.info())
